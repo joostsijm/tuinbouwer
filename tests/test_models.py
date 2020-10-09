6f8745edc05f72ed41dd3dde68c785e185b03c65
JoostSijm <joostsijm@gmail.com>
2020-10-09 19:30:00 +0200

    Add sensor log migration and improve model tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,27 +1,44 @@
 """Models tests"""
 
+from decimal import Decimal
+
 import pytest
 
 from tuinbouw_server_api import models
 
 def test_space_add(flask):
-    """Test adding empty space to database"""
+    """Test adding space to database"""
     space = models.Space()
-    space.name = "test space"
-
+    space.name = 'test space'
     models.db.session.add(space)
     models.db.session.commit()
-
     assert isinstance(space.id, int), "The id should be an int"
     assert isinstance(space.name, str), "The name should be a str"
+    assert space.name == 'test space', "The name should be 'test space'"
 
 def test_plant_add(flask):
-    """Test adding empty space to database"""
-    space = models.Space()
-    space.name = "test space"
-
-    models.db.session.add(space)
+    """Test adding plant to database"""
+    plant = models.Plant()
+    plant.name = 'test plant'
+    plant.harvest = 1
+    plant.number = 1
+    models.db.session.add(plant)
     models.db.session.commit()
+    assert isinstance(plant.id, int), "The id should be an int"
+    assert isinstance(plant.name, str), "The name should be a str"
+    assert isinstance(plant.harvest, int), "The harvest should be an int"
+    assert isinstance(plant.number, int), "The number should be an int"
+    assert plant.name == 'test plant', "The name should 'test plant'"
 
-    assert isinstance(space.id, int), "The id should be an int"
-    assert isinstance(space.name, str), "The name should be a str"
+def test_sensor_log_add(flask):
+    """Test adding sensor log to database"""
+    sensor_log = models.SensorLog()
+    sensor_log.temperature = 19.3
+    sensor_log.humidity = 30
+    sensor_log.power = 353.34
+    models.db.session.add(sensor_log)
+    models.db.session.commit()
+    assert isinstance(sensor_log.id, int), "Sensor log id should be an int"
+    assert isinstance(sensor_log.temperature, Decimal), "Sensor log temperature should be an int"
+    assert isinstance(sensor_log.humidity, int), "Sensor log humidity should be an int"
+    assert isinstance(sensor_log.power, Decimal), "Sensor log power should be an int"

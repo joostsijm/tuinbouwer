80d5c60a8e868f95d7c3d430ce8e7035ee9ea884
JoostSijm <joostsijm@gmail.com>
2020-10-18 16:08:50 +0200

    Improve API sending data

--- src/tuinbouwer_sensor/api.py
+++ src/tuinbouwer_sensor/api.py
@@ -10,20 +10,20 @@ from tuinbouwer_sensor import BASE_URL, HEADERS, LOGGER
 def post_sensor_log(sensor_log):
     """Download item id"""
     tries = 1
-    html = ''
-    while not html and tries <= 3:
+    while tries <= 3:
         try:
             response = requests.post(
                 '{}{}'.format(BASE_URL, 'sensor_api/v1/'),
                 headers=HEADERS,
                 data=sensor_log,
             )
+            return response.get_json()
         except requests.exceptions.ConnectionError as error:
             LOGGER.error(error)
-            LOGGER.info("Trying again to POST sensor log")
-        if response:
-            json = response.get_json()
-        else:
-            time.sleep(5)
-            tries += 1
-    return json
+            LOGGER.info("Connection error while POST sensor log")
+        except requests.exceptions.MissingSchema as error:
+            LOGGER.error(error)
+            LOGGER.info("Something is wrong with the URL")
+        LOGGER.info("Trying again to POST sensor log")
+        time.sleep(5)
+        tries += 1
